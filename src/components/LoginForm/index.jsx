44d72a1c3c542e611032355166baa5cacc486a56
Tokov <flyingcatsad@gmail.com>
2021-10-29 17:57:19 +0300

    Add regexps

--- src/components/LoginForm/index.jsx
+++ src/components/LoginForm/index.jsx
@@ -35,11 +35,30 @@ class LoginForm extends Component {
   handleSubmit = (event) => {
     event.preventDefault();
 
-    console.log(
-      `${this.state.login} ${this.state.password} ${this.state.remember}`
-    );
+    const regexEmail = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/gm;
+    const regexPass =
+      /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/gm;
 
-    this.handleReset();
+    if (
+      regexEmail.test(this.state.login) &&
+      regexPass.test(this.state.password)
+    ) {
+      console.log(
+        `${this.state.login} ${this.state.password} ${this.state.remember}`
+      );
+
+      this.handleReset();
+    }
+
+    if (!regexEmail.test(this.state.login)) {
+      alert("Имеил должен быть: текст@текст.домен");
+    }
+
+    if (!regexPass.test(this.state.password)) {
+      alert(
+        "Пароль должен иметь: одну заглавную, одну прописную буквы, одну цифру и один специальный символ"
+      );
+    }
   };
 
   render() {
@@ -47,7 +66,7 @@ class LoginForm extends Component {
       <form onSubmit={this.handleSubmit}>
         <h1>LOGIN TO YOUR ACCOUNT</h1>
         <Inputs state={this.state} handleChange={this.handleChange} />
-        <Buttons handleSubmit={this.handleSubmit} />
+        <Buttons />
       </form>
     );
   }
