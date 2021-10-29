8fb1f0362102eae986803dd108166da54a31592b
Tokov <flyingcatsad@gmail.com>
2021-10-29 16:36:44 +0300

    Add Buttons component

---
+++ src/components/LoginForm/Buttons/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Button = () => {
+  return (
+    <div>
+      <a href="/">Forgot Password</a>
+      <button type="submit">LOGIN</button>
+      <button type="submit">Sign in with Google</button>
+    </div>
+  );
+};
+
+export default Button;
