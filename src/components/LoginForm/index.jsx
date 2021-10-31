4371ba7ac510c03b23b623e6bb46dfd082c42b69
Tokov <flyingcatsad@gmail.com>
2021-10-31 13:12:00 +0200

    Change regExp logic

--- src/components/LoginForm/index.jsx
+++ src/components/LoginForm/index.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Buttons from "./Buttons";
-import Inputs from "./Inputs";
+import Input from "./Input";
 // import PropTypes from "prop-types";
 
 class LoginForm extends Component {
@@ -39,25 +39,25 @@ class LoginForm extends Component {
     const regexPass =
       /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/gm;
 
-    if (
-      regexEmail.test(this.state.login) &&
-      regexPass.test(this.state.password)
-    ) {
+    const verdictEmail = regexEmail.test(this.state.login);
+    const verdictPassword = regexPass.test(this.state.password);
+
+    if (!verdictEmail) {
+      alert("Имеил должен быть: текст@текст.домен");
+    }
+
+    if (!verdictPassword) {
+      alert(
+        "Пароль должен иметь: одну заглавную, одну прописную буквы, одну цифру и один специальный символ"
+      );
+    }
+
+    if (verdictEmail && verdictPassword) {
       console.log(
         `${this.state.login} ${this.state.password} ${this.state.remember}`
       );
 
       this.handleReset();
-    }
-
-    if (regexEmail.test(this.state.login) === false) {
-      alert("Имеил должен быть: текст@текст.домен");
-    }
-
-    if (regexPass.test(this.state.password) === false) {
-      alert(
-        "Пароль должен иметь: одну заглавную, одну прописную буквы, одну цифру и один специальный символ"
-      );
     }
   };
 
@@ -65,7 +65,38 @@ class LoginForm extends Component {
     return (
       <form onSubmit={this.handleSubmit}>
         <h1>LOGIN TO YOUR ACCOUNT</h1>
-        <Inputs state={this.state} handleChange={this.handleChange} />
+
+        <Input
+          type="text"
+          name="login"
+          id="login"
+          value={this.state.login}
+          placeholder="Email"
+          required={true}
+          handleChange={this.handleChange}
+        />
+
+        <Input
+          type="password"
+          name="password"
+          id="password"
+          value={this.state.password}
+          placeholder="Password"
+          required={true}
+          handleChange={this.handleChange}
+        />
+
+        <Input
+          type="checkbox"
+          name="remember"
+          id="remember"
+          checked={this.state.remember}
+          placeholder="Email"
+          required={false}
+          handleChange={this.handleChange}
+          labelAfter="Remember me"
+        />
+
         <Buttons />
       </form>
     );
