f75e764a1399b2c8f24d40ae15fa57f946cb5715
Tokov <flyingcatsad@gmail.com>
2021-10-31 14:10:02 +0200

    Refactor Input

--- src/components/LoginForm/Input/index.jsx
+++ src/components/LoginForm/Input/index.jsx
@@ -1,23 +1,12 @@
 import React from "react";
 
 const Input = ({ value, handleChange, labelBefore, labelAfter, ...rest }) => {
-  console.log(rest);
-
   return (
     <div>
       <label>
         {labelBefore}
 
-        <input
-          {...rest}
-          // type={type}
-          // name={name}
-          // id={id}
-          value={value}
-          // placeholder={placeholder}
-          // required={required}
-          onChange={handleChange}
-        />
+        <input value={value} onChange={handleChange} {...rest} />
 
         {labelAfter}
       </label>
